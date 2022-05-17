c1c52f1e71c2eed9b1fd28b576221d48d7ff8c37
Oswaldo Mendez <oswaluis8@gmail.com>
2022-05-17 16:37:14 -0300

    subida a la nube

---
+++ app/js/script.js
@@ -0,0 +1,29 @@
+const btnHamburger = document.getElementById('btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function(){
+    
+    if(header.classList.contains('open')){//cierro menu
+        header.classList.remove('open')
+        body.classList.remove('no-scroll')
+        fadeElements.forEach(function(element){
+            element.classList.remove('fade-in')
+            element.classList.add('fade-out')
+        })
+        
+        
+    }else{//abre menu
+        header.classList.add('open')
+        body.classList.add('no-scroll')
+        fadeElements.forEach(function(element){
+            element.classList.remove('fade-out')
+            element.classList.add('fade-in')
+        })
+      
+    }
+    
+})
+
