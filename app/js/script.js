7d6b2c7eb1d7a38fa9910e7b3043b00575c891a9
Oswaldo Mendez <oswaluis8@gmail.com>
2022-06-30 13:00:18 -0300

    actualizado

--- app/js/script.js
+++ app/js/script.js
@@ -27,3 +27,8 @@ btnHamburger.addEventListener('click', function(){
     
 })
 
+$(document).ready(function(){
+    $(".hamburger").click(function(){
+      $(this).toggleClass("is-active");
+    });
+  });
